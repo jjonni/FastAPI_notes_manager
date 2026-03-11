1db5621a414f24ce114976390857621e5e821683
jjonni <9010171@mail.ru>
2026-03-11 12:19:34 +0300

    Create DAL module skeleton

--- backend/app/database/dals/dals.py
+++ backend/app/database/dals/dals.py
@@ -1,1 +1,29 @@
 # Здесь будет data access control layer
+
+from sqlalchemy.orm import Session
+
+from backend.app.database.models.models import *
+
+class UserDAL:
+    def __init__(self, session: Session):
+        self.db_session = session
+
+    ...
+
+class NoteDAL:
+    def __init__(self, session: Session):
+        self.db_session = session
+
+    ...
+
+class TagDAL:
+    def __init__(self, session: Session):
+        self.db_session = session
+
+    ...
+
+class NoteTagDAL:
+    def __init__(self, session: Session):
+        self.db_session = session
+
+    ...
