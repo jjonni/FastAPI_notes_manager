5c0df9529b43f63d404c3d1a49f5e818742e7f35
jjonni <9010171@mail.ru>
2026-03-11 19:52:48 +0300

    Add first small function to DAL

--- backend/app/database/dals/dals.py
+++ backend/app/database/dals/dals.py
@@ -1,29 +1,13 @@
 # Здесь будет data access control layer
+# Но без классов, чисто на функциях
+# Возможно впоследствии разобью на несколько файлов, каждый из которых будет относиться к соответствующей ORM модели
 
 from sqlalchemy.orm import Session
 
 from backend.app.database.models.models import *
 
-class UserDAL:
-    def __init__(self, session: Session):
-        self.db_session = session
-
-    ...
-
-class NoteDAL:
-    def __init__(self, session: Session):
-        self.db_session = session
-
-    ...
-
-class TagDAL:
-    def __init__(self, session: Session):
-        self.db_session = session
-
-    ...
-
-class NoteTagDAL:
-    def __init__(self, session: Session):
-        self.db_session = session
-
-    ...
+def create_user(session: Session, name: str, surname: str, email: str, password_hash: str) -> User:
+    new_user = User(name=name, surname=surname, email=email, password_hash=password_hash)
+    session.add(new_user)
+    session.flush()
+    return new_user
